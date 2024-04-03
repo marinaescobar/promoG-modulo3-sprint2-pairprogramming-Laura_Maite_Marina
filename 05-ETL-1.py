ce5be2c5648412d4d1cd5b5a4df4c52e44343493
Laura Genesis <lauragenesis@Laura.localdomain>
2024-04-04 01:35:14 +0200

    Pair_ETL1

--- 05-ETL-1.py
+++ 05-ETL-1.py
@@ -3,3 +3,17 @@ from src import funciones as fun
 productos, clientes, ventas = fun.archivos()
 
 fun.exploracion(productos, clientes, ventas)
+
+clientes_limpio = fun.limpieza(clientes,productos,ventas)
+clientes_limpio
+
+nulos = clientes.isnull().sum()
+nulos
+
+tabla=fun.union(productos, clientes, ventas)
+tabla.head()
+
+final= fun.transformacion(tabla)
+final.head()
+
+
--- src/funciones.py
+++ src/funciones.py
@@ -43,4 +43,62 @@ def exploracion (df1 , df2 , df3):
                     print("") 
         print(f"\n-----------------------------\n")
     
+    return
+
+def limpieza (df2,df1,df3):
+    print("LIMPIEZA DE CLIENTES:")
+    df2[["email", "gender", "Address"]] = df2[["email", "gender", "Address"]].fillna("unknown")
+    df2["City"] = df2["City"].fillna("Madrid")
+    df2["Country"]= df2["Country"].fillna("Spain")
+    df2['full_name'] = df2['first_name'] + ' ' + df2['last_name']
+
+    df1['ID_Producto'] = df1['ID']
+    df3['id'] = df3['ID_Cliente']
     
+    return df2,df1,df3
+
+def union(df1,df2,df3):
+
+    
+
+    df_ventas_productos = pd.merge(df3, df1, on='ID_Producto', how='left')
+    tabla_unica = pd.merge(df_ventas_productos, df2, on='id', how='left')
+    #return tabla_unica
+    return tabla_unica
+
+
+def transformacion(tabla_unica):
+
+    tabla_unica = tabla_unica.drop(columns=['first_name', 'last_name', 'ID',"id"])
+
+    columnas_ordenadas = ['ID_Cliente', 'full_name','email', 'gender', 'City', 
+                          'Country', 'Address','ID_Producto', 'Nombre_Producto', 
+                          'Categoría', 'Origen', 'Descripción','Fecha_Venta', 
+                          'Cantidad','Precio', 'Total']
+    tabla_final= tabla_unica[columnas_ordenadas]
+    
+    #Cambiar nombre de columnas a español
+    mapeo_columnas = {
+    'ID_Cliente': 'ID Cliente',
+    'full_name': 'Nombre completo',
+    'email': 'Correo electrónico',
+    'gender': 'Género',
+    'City': 'Ciudad',
+    'Country': 'País',
+    'Address': 'Dirección',
+    'ID_Producto': 'ID Producto',
+    'Nombre_Producto': 'Nombre del producto',
+    'Categoría': 'Categoría',
+    'Origen': 'Origen',
+    'Descripción': 'Descripción',
+    'Fecha_Venta': 'Fecha de venta',
+    'Cantidad': 'Cantidad',
+    'Precio': 'Precio',
+    'Total': 'Total'
+}
+    tabla_final = tabla_final.rename(columns=mapeo_columnas)
+
+    return tabla_final
+
+
+# %%
